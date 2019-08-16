814f0395fb15a046a383858019b36c795a4e699d
Sid Hackney <snackney@gmail.com>
2019-08-16 14:03:34 -0400

    List of biography info loads and renders, truncated to item 5, 'favorite food'

--- script.js
+++ script.js
@@ -5,24 +5,35 @@ const data = $.parseJSON($.ajax({
   async: false
 }).responseText);
 
-// Store relevant character details as the fields they will be used for
-const details = {
-  name:data.name,
-  about:data.about,
-  followers:data.animeography.length,
-  following:data.mangaography.length,
-  image_url:data.image_url
-};
-
-// Insert main image as profile picture
-$('#profile-img').prepend(`<img src=${details.image_url}>`);
-
 // Fetch gallery images
 const pictures = $.parseJSON($.ajax( {
   url: 'https://api.jikan.moe/v3/character/246/pictures',
   dataType: "json",
   async: false
 }).responseText);
+
+// Store relevant character details as the fields they will be used for
+const details = {
+  name:data.name,
+  about:data.about.split('\n'),
+  followers:data.animeography.length,
+  following:data.mangaography.length,
+  image_url:data.image_url
+};
+
+
+// Insert main image as profile picture
+$('#profile-img').prepend(`<img src=${details.image_url}>`);
+$('#name').prepend(`${details.name}`);
+$('#post-count').prepend(`${pictures.pictures.length}`);
+$('#follower-count').prepend(`${details.followers}`);
+$('#following-count').prepend(`${details.following}`);
+
+for (let i=0; i<6; i++) {
+  $('#detail-list').append(`<li>${details.about[i]}</li>`);
+}
+
+
 
 console.log(details);
 console.log(pictures.pictures.length);
