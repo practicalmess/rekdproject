3b70e29e11c05bfba2f296b12d67736d7811db36
Sid Hackney <snackney@gmail.com>
2019-08-16 13:40:31 -0400

    Profile picure renders properly, started overall positioning

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+// Fetch character data by ID
 const data = $.parseJSON($.ajax({
   url: 'https://api.jikan.moe/v3/character/246',
   dataType: "json",
   async: false
 }).responseText);
+
+// Store relevant character details as the fields they will be used for
 const details = {
   name:data.name,
   about:data.about,
@@ -10,10 +13,16 @@ const details = {
   following:data.mangaography.length,
   image_url:data.image_url
 };
+
+// Insert main image as profile picture
+$('#profile-img').prepend(`<img src=${details.image_url}>`);
+
+// Fetch gallery images
 const pictures = $.parseJSON($.ajax( {
   url: 'https://api.jikan.moe/v3/character/246/pictures',
   dataType: "json",
   async: false
 }).responseText);
+
 console.log(details);
 console.log(pictures.pictures.length);
